7e069c76f152dcfdb924975556aa57cb92c1bbee
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-01 18:07:42 +0300

    Halfway done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,23 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+
+    def __init__(self, name, artist, genre):
+        Song.add_song_to_count()
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_to_genres(self.genre)
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        return cls.genres
+
+    
+            
